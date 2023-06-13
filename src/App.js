35c38da0696dd46cd843092accbf3fb5580d4da3
Aaron Gesmer <aaron.gesmer@gmail.com>
2023-06-13 14:42:42 +0000

    temp stage experience timeline

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Typography } from 'antd';
+import { Layout, Typography, Timeline } from 'antd';
 
 const { Header, Content, Footer } = Layout;
 const { Title, Text } = Typography;
@@ -14,8 +14,24 @@ function App() {
         <Content style={contentStyle}>
           <Title level={2}>Hello, World!</Title>
           <Text style={textStyle}>There are so many things in this world that interest me, but I am particularly passionate about playing the guitar, cooking, and especially technology. I love watching the NBA, and am a huge fan of the Minnesota Timberwolves! </Text>
-          <div>
+          <div style={{ border: '1px solid hotpink', maxWidth: 400 }}>
             <Title level={3}>Experience</Title>
+            <Timeline
+              items={[
+                {
+                  children: 'Create a services site 2015-09-01',
+                },
+                {
+                  children: 'Solve initial network problems 2015-09-01',
+                },
+                {
+                  children: 'Technical testing 2015-09-01',
+                },
+                {
+                  children: 'Network problems being solved 2015-09-01',
+                },
+              ]}
+            />
           </div>
         </Content>
         <Footer style={footerStyle}>This site was built with React and Ant Design.</Footer>
