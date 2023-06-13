aebba85691f232401f29bf43d7b6405a906ea98d
Aaron Gesmer <aaron.gesmer@gmail.com>
2023-06-13 13:58:42 +0000

    add some content to site

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import './App.css';
-import Layout, { Header } from 'antd/es/layout/layout';
+import React from 'react';
+import { Layout, Typography } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <Header style={{ color: 'white' }}>Aaron Gesmer</Header>
+        <Header style={headerStyle}>
+          <Title style={titleStyle}>Aaron Gesmer</Title>
+        </Header>
+        <Content style={contentStyle}>
+          <Title level={2}>Hello, World!</Title>
+        </Content>
+        <Footer style={footerStyle}>This site was built with React and Ant Design.</Footer>
       </Layout>
     </div>
   );
 }
+
+const headerStyle = {
+  textAlign: 'center',
+  color: 'white',
+  height: 108,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#108ee9',
+};
+
+const titleStyle = {
+  color: 'inherit',
+};
+
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+};
+
+const footerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#7dbcea',
+};
 
 export default App;
