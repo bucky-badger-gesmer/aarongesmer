d775743d1930638693ed2623dd6bdef4382b86f4
Aaron Gesmer <aaron.gesmer@gmail.com>
2023-06-14 15:08:32 -0500

    portfolio is in decent spot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Layout, Typography, Timeline } from 'antd';
+import { Layout, Typography, Timeline, Card, Avatar } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact } from '@fortawesome/free-brands-svg-icons'
-import { AntDesignOutlined } from '@ant-design/icons';
+import { AntDesignOutlined, GithubOutlined, LinkedinOutlined, TwitterOutlined } from '@ant-design/icons';
 
 const { Header, Content, Footer } = Layout;
 const { Title, Text, Link } = Typography;
+const { Meta } = Card;
 
 function App() {
   return (
@@ -15,50 +16,67 @@ function App() {
           <Title style={titleStyle}>Aaron Gesmer</Title>
         </Header>
         <Content style={contentStyle}>
-          <Title level={2}>Hello, World!</Title>
-          <Text>There are so many things in this world that interest me, but I am particularly passionate about playing the guitar, cooking, and especially technology. I love watching the NBA, and am a huge fan of the Minnesota Timberwolves! </Text>
-            <Title level={3}>Experience</Title>
-              <Timeline
-                mode='alternate'
-                style={timelineStyle}
-                items={[
-                  {
-                    children:
-                      <>
-                        <Title level={4}>May 13th 2017</Title>
-                        Graduated from the University of Wisconsin with a Bachelor of Arts in Computer Science and a Certificate in Mathematics
-                      </>,
-                  },
-                  {
-                    children:
-                      <>
-                        <Title level={4}>June 12th 2017</Title>
-                        Software Engineer I at <Link>Cleo</Link>
-                      </>,
-                  },
-                  {
-                    children:
-                      <>
-                        <Title level={4}>June 12th 2018</Title>
-                        Test Engineer I at <Link>Cleo</Link>
-                      </>,
-                  },
-                  {
-                    children:
-                      <>
-                        <Title level={4}>November 18th 2019</Title>
-                        Frontend Engineer at <Link>STEALTHbits (now Netwrix)</Link>
-                      </>,
-                  },
-                  {
-                    children:
-                      <>
-                        <Title level={4}>August 16th 2021</Title>
-                        Software Engineer - P2 at <Link>Mission Lane</Link>
-                      </>
-                  },
-                ]}
+          <Card
+            style={{ marginTop: 24 }}
+            cover={
+              <img
+                alt="example"
+                src="/avatar.jpeg"
               />
+            }
+            actions={[
+              <LinkedinOutlined />,
+              <GithubOutlined />,
+              <TwitterOutlined />,
+            ]}
+          >
+            <Meta
+              title="Hello, World!"
+              description="There are so many things in this world that interest me, but I am particularly passionate about playing the guitar, cooking, and especially technology. I love watching the NBA, and am a huge fan of the Minnesota Timberwolves! "
+            />
+          </Card>
+          <Title level={3}>Experience</Title>
+          <Timeline
+            mode='alternate'
+            style={timelineStyle}
+            items={[
+              {
+                children:
+                  <>
+                    <Title level={4}>May 13th 2017</Title>
+                    Graduated from the University of Wisconsin with a Bachelor of Arts in Computer Science and a Certificate in Mathematics
+                  </>,
+              },
+              {
+                children:
+                  <>
+                    <Title level={4}>June 12th 2017</Title>
+                    Software Engineer I at <Link>Cleo</Link>
+                  </>,
+              },
+              {
+                children:
+                  <>
+                    <Title level={4}>June 12th 2018</Title>
+                    Test Engineer I at <Link>Cleo</Link>
+                  </>,
+              },
+              {
+                children:
+                  <>
+                    <Title level={4}>November 18th 2019</Title>
+                    Frontend Engineer at <Link>STEALTHbits (now Netwrix)</Link>
+                  </>,
+              },
+              {
+                children:
+                  <>
+                    <Title level={4}>August 16th 2021</Title>
+                    Software Engineer - P2 at <Link>Mission Lane</Link>
+                  </>
+              },
+            ]}
+          />
         </Content>
         <Footer style={footerStyle}>This site was built with React <FontAwesomeIcon icon={faReact} /> and Ant Design <AntDesignOutlined /></Footer>
       </Layout>
@@ -69,6 +87,7 @@ function App() {
 const headerStyle = {
   position: 'sticky',
   top: 0,
+  zIndex: 1,
   textAlign: 'center',
   color: 'white',
   height: 108,
