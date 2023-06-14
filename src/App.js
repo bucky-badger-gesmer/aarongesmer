bf187da082b4c25df5b23331ca14463c052f7042
Aaron Gesmer <aaron.gesmer@gmail.com>
2023-06-14 14:38:00 -0500

    app has more styling and icons in footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Layout, Typography, Timeline } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact } from '@fortawesome/free-brands-svg-icons'
+import { AntDesignOutlined } from '@ant-design/icons';
 
 const { Header, Content, Footer } = Layout;
 const { Title, Text } = Typography;
@@ -13,28 +16,27 @@ function App() {
         </Header>
         <Content style={contentStyle}>
           <Title level={2}>Hello, World!</Title>
-          <Text style={textStyle}>There are so many things in this world that interest me, but I am particularly passionate about playing the guitar, cooking, and especially technology. I love watching the NBA, and am a huge fan of the Minnesota Timberwolves! </Text>
-          <div style={{ border: '1px solid hotpink', maxWidth: 400 }}>
+          <Text>There are so many things in this world that interest me, but I am particularly passionate about playing the guitar, cooking, and especially technology. I love watching the NBA, and am a huge fan of the Minnesota Timberwolves! </Text>
             <Title level={3}>Experience</Title>
-            <Timeline
-              items={[
-                {
-                  children: 'Create a services site 2015-09-01',
-                },
-                {
-                  children: 'Solve initial network problems 2015-09-01',
-                },
-                {
-                  children: 'Technical testing 2015-09-01',
-                },
-                {
-                  children: 'Network problems being solved 2015-09-01',
-                },
-              ]}
-            />
-          </div>
+              <Timeline
+                style={timelineStyle}
+                items={[
+                  {
+                    children: 'Create a services site 2015-09-01',
+                  },
+                  {
+                    children: 'Solve initial network problems 2015-09-01',
+                  },
+                  {
+                    children: 'Technical testing 2015-09-01',
+                  },
+                  {
+                    children: 'Network problems being solved 2015-09-01',
+                  },
+                ]}
+              />
         </Content>
-        <Footer style={footerStyle}>This site was built with React and Ant Design.</Footer>
+        <Footer style={footerStyle}>This site was built with React <FontAwesomeIcon icon={faReact} /> and Ant Design <AntDesignOutlined /></Footer>
       </Layout>
     </div>
   );
@@ -58,13 +60,16 @@ const titleStyle = {
 const contentStyle = {
   textAlign: 'center',
   minHeight: 2400,
+  maxWidth: 700,
   color: '#fff',
+  marginLeft: 'auto',
+  marginRight: 'auto',
   paddingLeft: 24,
   paddingRight: 24,
 };
 
-const textStyle = {
-  textAlign: 'justify',
+const timelineStyle = {
+  textAlign: 'left',
 };
 
 const footerStyle = {
