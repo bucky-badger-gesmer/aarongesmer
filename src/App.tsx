6e72df183947c559140f79430146bc2d67e9070b
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2023-06-20 15:09:36 -0500

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Layout, Typography } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReact } from "@fortawesome/free-brands-svg-icons";
 import { AntDesignOutlined } from "@ant-design/icons";
-import Profile from "./components/Profile";
+import { Profile } from "./components";
 
 const { Header, Footer } = Layout;
 const { Title } = Typography;
@@ -12,39 +12,19 @@ const App = () => {
   return (
     <div className="App">
       <Layout>
-        <Header style={headerStyle}>
-          <Title style={titleStyle}>Aaron Gesmer</Title>
+        <Header className="sticky top-0 z-10 shadow-lg text-center md:text-left">
+          <Title className="mt-2" style={{ color: "white" }}>
+            Aaron Gesmer
+          </Title>
         </Header>
         <Profile />
-        <Footer style={footerStyle}>
+        <Footer className="text-center bg-sky-500 text-white">
           This site was built with React <FontAwesomeIcon icon={faReact} /> and
           Ant Design <AntDesignOutlined />
         </Footer>
       </Layout>
     </div>
   );
-};
-
-const headerStyle: React.CSSProperties = {
-  position: "sticky",
-  top: 0,
-  zIndex: 1,
-  textAlign: "center",
-  color: "white",
-  height: 108,
-  paddingInline: 50,
-  lineHeight: "64px",
-  backgroundColor: "#108ee9",
-};
-
-const titleStyle = {
-  color: "inherit",
-};
-
-const footerStyle: React.CSSProperties = {
-  textAlign: "center",
-  color: "#fff",
-  backgroundColor: "#7dbcea",
 };
 
 export default App;
