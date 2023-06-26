8d197e2bcb9fa32fcebcf12993794d9e7e526cd0
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2023-06-26 09:28:00 -0500

    rm React import from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Layout, Typography } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReact } from "@fortawesome/free-brands-svg-icons";
