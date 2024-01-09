eac825272f220be49acff35d5d7aa7bb66c6e660
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2024-01-09 08:16:42 -0600

    feat: move over to nextjs/tailwind/ionic

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import Avatar from "./Avatar";
-import Experience from "./Experience";
-import Profile from "./Profile";
-import Projects from "./Projects";
-import Watches from "./Watches";
-
-export { Avatar, Experience, Profile, Projects, Watches };
